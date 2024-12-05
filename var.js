9b9913c3b5da5747bd24a3dae706f6cc8b45894e
PranavMishra07 <pranav072003@gmail.com>
2024-12-05 13:37:48 +0530

    l

---
+++ var.js
@@ -0,0 +1,17 @@
+var a = 10
+var a = 12
+
+console.log(a);
+ 
+let b=2
+b=4
+
+console.log(b);
+
+const c=89
+console.log(c);
+
+ let num_1 = 10
+ let num_2 = 90
+ 
+ console.log("Addition of two number : ",num_1+num_2)
