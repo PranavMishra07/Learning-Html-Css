08003d3d3c6992e45584233aa5b1410ec157b805
PranavMishra07 <pranav072003@gmail.com>
2024-12-06 19:58:55 +0530

    p

---
+++ conditional.js
@@ -0,0 +1,63 @@
+// let n=parseInt(prompt("Enter any number : "))
+// if(n<0){
+//     console.log("Negative",n)
+// }
+// else if(n>0){
+//     console.log("Positive",n)
+// }
+// else if(n=0){
+//     console.log("Neutral",n)
+// }
+
+// if(n=1){
+//     alert("Ayush");
+// }
+
+// else if(n=2){
+//     alert("Anuj");
+// }
+// else if(n=3){
+//     alert("Vikash");
+// }
+// else{
+//     alert("Wrong value")
+// }
+
+
+// let age=parseInt(prompt("enter your age"))
+
+// if(age>18){
+//     let n
+//     parseInt(prompt("Enter 1 for indian"))
+//     if(n=1){
+//         alert("You are elegible")
+//     }
+//     else{
+//         alert("you are not Indian")
+//     }
+// }
+// else{
+//     alert("You are child")
+// }
+
+// let n= parseInt(prompt("Enter a number : "))
+
+// switch(n){
+//     case 1:
+//         alert("c++")
+//         break;
+
+//     case 2:
+//         alert("js")
+//         break;    
+
+//     default :
+//     alert("wrong VAlue")    
+// }
+
+var a =1
+do{
+    console.log(a);
+    a++
+}
+while(a<6)
--- var.js
+++ var.js
@@ -1,17 +1,29 @@
-var a = 10
-var a = 12
+// var a = 10
+// var a = 12
 
-console.log(a);
+// console.log(a);
  
-let b=2
-b=4
+// let b=2
+// b=4
 
-console.log(b);
+// console.log(b);
 
-const c=89
-console.log(c);
+// const c=89
+// console.log(c);
 
- let num_1 = 10
- let num_2 = 90
+//  let num_1 = 10
+//  let num_2 = 90
  
- console.log("Addition of two number : ",num_1+num_2)
+//  console.log("Addition of two number : ",num_1+num_2)
+
+
+ let num1=parseInt(prompt("ENter Number 1 : "))
+//  let num2=parseInt(prompt("Enter Number 2 : "))
+  
+ if(num1%2==0){
+   
+    console.log("Area of Circle ",3.14*num1*num1)
+ }
+else{
+     console.log("Cube is ",num1*num1*num1)
+ }
