8d4b02a2021ed67b3fbf9c13c8929e3e80be350b
PranavMishra07 <pranav072003@gmail.com>
2024-12-08 00:38:21 +0530

    k

--- turnary.js
+++ turnary.js
@@ -25,13 +25,49 @@
 // for(let n=1; n<=10; n++){
 //     console.log(n);
 // }
-for(let n=15; n>=1; n--){
-    
-    if (n==10){
-        console.log(n*n)
+
+// for(let n=15; n>=1; n--){
+
+//   if(n==10 || n==5){
+//         console.log(n*n)
+ 
+//     }
+//     else{
+//         console.log(n)
+//     }
+ 
+// }
+// for(let b=10; b<=20;b++){
+//     if (b%2==0){
+//         console.log(b)
+        
+//     }
+// }
+
+// let fact=1
+// let n=5
+// for(let i=1;i<=n;i++){
+//     fact=fact*i
+  
+// }
+  
+// console.log(fact); 
+// let n=parseInt(prompt("Enter A Number :"))
+// let sum=0,r
+// while(n!=0){
+// r=n%10
+// sum=sum+r
+// n=n/10
+// }
+// console.log(sum);
+// document.write(sum)
+let n,sum=0,r
+while(n!=0){
+    r=n%10
+    if(r%2==0){
+        sum=sum+r
     }
-    else if(n==5){
-        console.log(n*n)
-    }
-    console.log (n)
+   console.log(sum);
+   
 }
+
