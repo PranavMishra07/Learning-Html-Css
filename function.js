924584227defe758740214eacd74db7d5f383840
PranavMishra07 <pranav072003@gmail.com>
2024-12-10 14:18:09 +0530

    k

---
+++ function.js
@@ -0,0 +1,7 @@
+function Demo(){
+    let a=10
+    let b=20
+    let c=a+b;
+    console.log("Addition is : ",c)
+}
+Demo()
---
+++ nestedfor.js
@@ -0,0 +1,14 @@
+for (let a = 2; a <=3;a++){
+    for(b=1;b<=10;b++){
+        console.log(a*b);
+    }
+   
+}
+
+
+for(let r=1;r<=5;r++){
+   for(c=1;c<=r;c++){
+    document.write("*")
+    }
+    document.write("<br>")
+   }
