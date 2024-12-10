1ab3a9ef44cc1b5638c61c73e0e1308dd88cee0b
PranavMishra07 <pranav072003@gmail.com>
2024-12-10 19:59:21 +0530

    l

--- function.js
+++ function.js
@@ -1,7 +1,44 @@
-function Demo(){
-    let a=10
-    let b=20
-    let c=a+b;
-    console.log("Addition is : ",c)
+// function Demo(){
+//     let a=10
+//     let b=20
+//     let c=a+b;
+//     console.log("Addition is : ",c)
+// }
+// Demo()
+
+
+// function Square(){
+//     let i
+//     for(i=10;i>=1;i--){
+//         if(i%2!=1){
+//             console.log(i*i)
+//         }
+//     }
+// }
+// Square()
+
+
+
+
+//with argument and no return
+// function Myfun(a){
+//     console.log(a+a)
+// }
+// Myfun(8)
+
+// function Myfun(b){
+//     console.log(b+b)
+// }
+// let b=parseInt(prompt("enter any Number : "))
+// Myfun(b)
+
+
+
+//write a programm to find area of circle radius taking from the user using function type 2 
+
+
+function Area(r){
+    console.log(2*3.14*r)
 }
-Demo()
+let r=parseInt(prompt("Enter Radius : "))
+Area(r)
