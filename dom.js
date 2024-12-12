f9b7ab57b523e0282d6422d7b42530e303fa1ef6
PranavMishra07 <pranav072003@gmail.com>
2024-12-12 16:41:01 +0530

    i

---
+++ dom.js
@@ -0,0 +1,10 @@
+let head1 = document.getElementById("head")
+head1.innerHTML=("Welcome to Home")
+
+let heading1=document.getElementsByClassName('heading')
+heading1[0].innerHTML="Pranav"
+heading1[3].innerHTML="Hello Guys"
+
+
+let list1=document.getElementsByTagName('li')
+list1[1].innerHTML="Pranav"
