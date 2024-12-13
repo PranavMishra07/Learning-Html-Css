336dcaf82ac2a8453a8f80d7fbf5080bd5a93252
PranavMishra07 <pranav072003@gmail.com>
2024-12-13 19:56:59 +0530

    k

---
+++ jsimg.js
@@ -0,0 +1,53 @@
+// let img = document.getElementById("image")
+// img.src="image/slack_2584687.png"
+// let h= document.getElementById("head")
+// h.style.color="blue"
+// h.style.fontSize="80px"
+// h.style.backgroundColor="yellow"
+// h.innerHTML="<i>All is well</i>"
+// let head2 =document.getElementById('head2')
+// head2.style.cssText="background-color:red; color:white; padding:20px; text-align:center; "
+
+// head2.textContent="<i>All is well</i>"
+
+// document.getElementById('para').innerHTML="paragraph"
+
+
+// let color=document.getElementsByClassName("Order")
+// // color[0].style.cssText="background-color: red;"
+
+// for(let i=0;i<=10;i++){
+//     if(n%2!=0){
+//         color[0].style.cssText="background-color:red; color:blue; "
+//     }
+// }
+
+// let lis=document.getElementsByTagName('li')
+// for(let i=0;i<=10;i++){
+//     if(i%2==0){
+//         lis[i].style.backgroundColor="yellow"
+//     }
+// }
+// let b = document.querySelector("#head3")
+// b.style.color = 'blue'
+// let c = document.querySelector(".head4")
+// c.style.color = 'red'
+
+// let a = document.getElementsByClassName(".load")
+// a[0].innerHTML = "red"
+
+
+
+// let x=document.querySelector(".load")
+// x.style.color="red"
+
+
+let u=document.querySelectorAll('h1')
+u[0].style.color="yellow"
+
+
+
+
+
+
+
