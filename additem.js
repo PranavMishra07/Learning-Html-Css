5d95472bbada62a46aa4911aec1e6ae0016af965
PranavMishra07 <pranav072003@gmail.com>
2024-12-16 22:27:28 +0530

    k

---
+++ additem.js
@@ -0,0 +1,23 @@
+function minus( ){
+    let a= 0
+   
+ document.querySelector("#h1").innerHTML="-1"
+   
+   
+}
+// function plus( ){
+//     let a= 0
+   
+//  document.querySelector("#h1").innerHTML="1"
+   
+   
+// }
+let a= document.querySelector("#h1").innerHTML="0"
+if(onclick=plus){
+ 
+ document.querySelector("#h1").innerHTML=  a=a+1
+ if(a<=100){
+    document.querySelector("#h1").innerHTML=  a
+ }
+ 
+}
