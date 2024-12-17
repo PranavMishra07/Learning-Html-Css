6990c5991b77ceecb741071ca53cfa21e56b421f
PranavMishra07 <pranav072003@gmail.com>
2024-12-17 22:57:39 +0530

    j

---
+++ flipkart.js
@@ -0,0 +1,28 @@
+function fun1(){
+    let img = document.getElementById('main')
+    img.src="flif/two.jpeg"
+}
+function fun2(){
+    let img = document.getElementById('main')
+    img.src="flif/three.jpeg"
+}
+function fun3(){
+    let img = document.getElementById('main')
+    img.src="flif/four.jpeg"
+}
+function fun4(){
+    let img = document.getElementById('main')
+    img.src="flif/five.jpeg"
+}
+function fun5(){
+    let img = document.getElementById('main')
+    img.src="flif/six.jpeg"
+}
+function fun6(){
+    let img = document.getElementById('main')
+    img.src="flif/seven.jpeg"
+}
+function fun7(){
+    let img = document.getElementById('main')
+    img.src="flif/eight.jpeg"
+}
