b399c9a40d296d412fe4b6facf8454ecac573f01
PranavMishra07 <pranav072003@gmail.com>
2024-12-18 15:10:31 +0530

    K

--- additem.js
+++ additem.js
@@ -1,23 +1,13 @@
-function minus( ){
-    let a= 0
-   
- document.querySelector("#h1").innerHTML=-1
-   
-   
-}
-// function plus( ){
-//     let a= 0
-   
-//  document.querySelector("#h1").innerHTML="1"
-   
-   
-// }
-// let a= document.querySelector("#h1").innerHTML="0"
-if(onclick=plus){
- 
- document.querySelector("#h1").innerHTML=  a=a+1
- if(a<=100){
-    document.querySelector("#h1").innerHTML=  a
- }
- 
+function plus() {
+    let a = 0
+
+    let plus = document.querySelector('#input').innerHTML = a + 1
+    if (onclick == 1) {
+        plus.innerHTML = 3
+
+    }
+
+
+
+
 }
