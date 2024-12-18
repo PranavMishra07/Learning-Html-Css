701633364d78dd9649302f90b09ec241ca5717ed
PranavMishra07 <pranav072003@gmail.com>
2024-12-18 19:52:36 +0530

    l

---
+++ element.js
@@ -0,0 +1,21 @@
+let head_element = document.createElement('h1')
+head_element.textContent = "This is Head  h1 From Js"
+
+let bd = document.body
+
+bd.append(head_element)
+
+
+let head2 = document.createElement('h2')
+head2.textContent = "This is Head2 h2 from js"
+bd.append(head2)
+
+head_element.setAttribute('align', 'center')
+// head2.setAttribute('style', 'color:blue')
+head2.setAttribute('style', 'background-color:blue')
+
+bd.setAttribute('bgcolor', 'yellow')
+bd.removeAttribute('bgcolor')
+
+let img = document.getElementById('image')
+img.setAttribute('src', 'flif/four.jpeg')
