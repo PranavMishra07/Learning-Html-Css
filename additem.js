605e0aca336edbb87ded8e8eb152260bd6e31b76
PranavMishra07 <pranav072003@gmail.com>
2024-12-27 16:29:44 +0530

    s

--- additem.js
+++ additem.js
@@ -1,18 +1,34 @@
 
-let plus = document.querySelector('#plus')
-let minus = document.querySelector('#minus')
-let input = document.querySelector('#input')
-console.log(plus, minus, input)
+// let plus = document.querySelector('#plus')
+// let minus = document.querySelector('#minus')
+// let input = document.querySelector('#input')
+// console.log(plus, minus, input)
 
+// plus.addEventListener('click', () => {
+//     input.value = parseInt(input.value) + 1;
+// });
+// minus.addEventListener('click', () => {
+//     if (input.value <= 0) {
+//         input.value = 0;
+//     }
+//     else {
+//         input.value = parseInt(input.value) - 1;
+//     }
+
+// });
+let plus = document.querySelector("#plus")
+let minus = document.querySelector("#minus")
+let input = document.querySelector("input")
 plus.addEventListener('click', () => {
     input.value = parseInt(input.value) + 1;
-});
+})
 minus.addEventListener('click', () => {
     if (input.value <= 0) {
-        input.value = 0;
+        input.value = 0
     }
     else {
-        input.value = parseInt(input.value) - 1;
-    }
 
-});
+
+        input.value = parseInt(input.value) - 1
+    }
+})
